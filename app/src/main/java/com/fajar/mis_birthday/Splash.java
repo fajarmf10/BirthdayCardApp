60663f9c2240983483e0c054f4e99ad57590fb94
Heimdall Arkadious <harkadious@gmail.com>
2016-10-05 09:52:54 +0700

    Tinggal mindahin ke Values etc etc etc fak lelah

--- app/src/main/java/com/fajar/mis_birthday/Splash.java
+++ app/src/main/java/com/fajar/mis_birthday/Splash.java
@@ -8,15 +8,26 @@ package com.fajar.mis_birthday;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.Snackbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class Splash extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        View haha = findViewById(R.id.splashscreen);
+        Snackbar snekbar = Snackbar.make(haha, "Launching the apps, please wait...", Snackbar.LENGTH_LONG);
+        View viewnyasb = snekbar.getView();
+        TextView viewteks = (TextView) viewnyasb.findViewById(android.support.design.R.id.snackbar_text);
+        viewteks.setTextColor(Color.MAGENTA);
+        snekbar.show();
 
         int secondsDelayed = 1;
         new Handler().postDelayed(new Runnable() {
